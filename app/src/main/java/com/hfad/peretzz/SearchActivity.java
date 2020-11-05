f7820050743573ba2f3dcea3c045e5ea856c66c3
Admin <marip55598@gmail.com>
2020-11-05 17:54:35 +0300

    afac1

--- app/src/main/java/com/hfad/peretzz/SearchActivity.java
+++ app/src/main/java/com/hfad/peretzz/SearchActivity.java
@@ -1,8 +1,11 @@
 package com.hfad.peretzz;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        Toolbar toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+
     }
 }
